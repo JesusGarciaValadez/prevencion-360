04b6222a70ae93cf45dd35c1f6170f661fa80ab2
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-06-25 22:59:39 +0000

    Adidng original script for controling mobile menu

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,4 +1,5 @@
 //  @codekit-prepend "plugins.js";
+//  @codekit-prepend "scripts.js";
 /**
  *
  *  @function
@@ -30,43 +31,6 @@
 
     //  When page is finished loaded
     $( document ).ready( function () {
-        if ( $( '.stars li' ).exists() ) {
-            $( '.stars li' ).on( 'click', 'a', function ( e ) {
-                e.preventDefault();
-                e.stopPropagation();
 
-                $( '.stars li a' ).removeClass('Star-1').addClass('Star-2');
-
-                var _quality = $( e.currentTarget ).data( 'quality' ),
-                    _section = $( e.currentTarget ).parents( 'ul' ).data( 'section' ),
-                    _month   = $( e.currentTarget ).parents( 'ul' ).data( 'month' ),
-                    _year    = $( e.currentTarget ).parents( 'ul' ).data( 'year' ),
-                    _options = {
-                        data: {
-                            star:    _quality,
-                            section: _section,
-                            month:   _month,
-                            year:    _year,
-                        },
-                        type: 'POST'
-                    };
-                $( '.stars li' ).each( function ( index ) {
-                    if ( _quality >= ( index + 1 ) ) {
-                        $( '.stars li' ).eq( index ).children('a').removeClass('Star-2').addClass('Star-1');
-                    } else {
-                        return;
-                    }
-                } );
-                $.ajax( 'inicio/starPost', _options )
-                 .done( function () { } )
-                 .then( function ( response ) {
-                    response = $.parseJSON( response );
-                    if ( response.code === 'fail' ) {
-                        $( '.stars li a' ).removeClass('Star-1').addClass('Star-2');
-                    }
-                 } )
-                 .fail( function () { $( '.stars li a' ).removeClass('Star-1').addClass('Star-2'); } );
-            } );
-        }
     } );
 } ) ( jQuery, window, document );
---
+++ source/javascript/scripts.js
@@ -0,0 +1,101 @@
+var abrir = 0;
+var desp = 0;
+$(document).ready(function ()
+{
+
+    $('.axa_dropdown').on('click', function() {
+        if (abrir === 1) {
+            var id = $(this).attr('id');
+            //$('#dropdown_' + id).fadeOut();
+            abrir = 0;
+        } else if (abrir === 0) {
+            var id = jQuery(this).attr('id');
+            //jQuery('#dropdown_' + id).fadeIn();
+            abrir = 1;
+        }
+    });
+
+    $('#acotamiento').on("click", function() {
+        var id = $(this).attr('id');
+        $('#dropdown_' + id).fadeOut();
+        abrir = 0;
+    });
+
+    $("#cuerpo-gral").on("click", function() {
+        var id = $(this).attr('id');
+        $('#dropdown_' + id).fadeOut();
+        abrir = 0;
+    });
+
+
+    jQuery('.axa_dropdown').click(function() {
+        if($('#wrapper').innerWidth()>=979){
+            var id = jQuery(this).attr('id');
+            jQuery('#dropdown_'+id).slideToggle(400);
+        }
+    })
+    .mouseleave(function(){
+        if($('#wrapper').innerWidth()>=979){
+
+            var id = jQuery(this).attr('id');
+            jQuery('#dropdown_'+id).fadeOut(400);
+
+        }
+    }
+    );
+
+
+
+    $( "#iconMainMenu" ).click(function() {
+        $( "#axa_close_mainMenu" ).show();
+        if($('#wrapper').innerWidth()>=719){
+            $( "#navigationBar_container" ).animate({
+            right: "0",
+            width: "370px",
+            paddingLeft: "50px"
+            }, 800 );
+        }
+        else{
+            $( "#navigationBar_container" ).animate({
+            right: "0",
+            width: "270px",
+            paddingLeft: "50px"
+            }, 800 );
+        }
+
+
+    });
+
+    $('#axa_close_mainMenu').click(function(event) {
+        $(this).hide();
+        $( "#navigationBar_container" ).animate({
+        right: "0",
+        width: "0px",
+        paddingLeft: "0px"
+        }, 800 );
+    });
+
+    jQuery('.axa_dropdown').click(function() {
+        if($('#wrapper').innerWidth()<980){
+            var id = jQuery(this).attr('id');
+            jQuery('#dropdown_'+id).slideToggle(1000);
+        }
+    });
+
+    $('#carousel[data-mixed] ul').anoSlide({
+        items: 5,
+        speed: 500,
+        prev: 'a.prev[data-prev]',
+        next: 'a.next[data-next]',
+        lazy: false,
+        delay: 50
+    });
+
+    $(document).click(function(event)
+        {
+            var id = jQuery(this).attr('id');
+            //jQuery('#dropdown_'+id).fadeOut(400);
+        });
+
+
+});
